35993a93911803e5d14a510edc906d46c8b96d0c
Freedom Chedozie Chukwudume <freedom.chuks7@gmail.com>
2022-07-20 22:32:06 +0100

    fixed codeTypo

--- app/src/main/java/com/example/chucknorries/domain/usecases/base/FlowUseCase.kt
+++ app/src/main/java/com/example/chucknorries/domain/usecases/base/FlowUseCase.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 
-abstract class FlowUsecase<in Params, out T> {
+abstract class FlowUseCase<in Params, out T> {
 
     abstract val dispatcher:CoroutineDispatcher
 
